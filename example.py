010a7604b41019b6ac6a0e63e244c0f2054a0c4a
khuyentran1401 <khuyentran1476@gmail.com>
2021-03-02 16:23:19 -0600

    fix example

--- example.py
+++ example.py
@@ -3,11 +3,7 @@ from top_github_scraper import get_top_repo_urls, get_top_repos, get_top_contrib
 if __name__=='__main__':
     keyword = "machine learning"
     stop_page = 2
-    max_n_top_contributors = 2
-    sort_by = 'stars'
  
-    # get_top_repo_urls("machine learning", stop_page=2)
-    # get_top_repos(keyword="machine learning", stop_page=2)
-    # get_top_contributors(keyword, stop_page=stop_page)
+    get_top_contributors(keyword, stop_page=stop_page)
 
     get_top_users(keyword, stop_page=stop_page)
